b80e752487211823b444d7e065c6ffaf19417845
Divyanshu Singh <sdivyanshu124@gmail.com>
2024-03-23 10:08:54 +0530

    react project

---
+++ src/app/discussionData.js
@@ -0,0 +1,54 @@
+// discussionData.js
+
+const discussionPostsData = [
+  {
+    id: 1,
+    description:
+      "Michael Scott: A successful regional manager with a unique management style. With his charisma and humor, he leads the Dunder Mifflin Scranton branch to success.",
+    name: "Michael Scott",
+    minutesAgo: 3,
+    profilePic:
+      "https://i.pinimg.com/originals/5e/f5/2e/5ef52e086b8d9a8a3a2f08a7851538f2.png",
+    likes: "1.2K",
+    views: "5.6K",
+    comments: "120",
+  },
+  {
+    id: 2,
+    description:
+      "Tony Stark, also known as Iron Man, is a genius inventor and billionaire who dominates the stock market with his advanced technology and strategic decisions.",
+    name: "Tony Stark",
+    minutesAgo: 14,
+    profilePic:
+      "https://i.pinimg.com/originals/5e/f5/2e/5ef52e086b8d9a8a3a2f08a7851538f2.png",
+    likes: "9.5K",
+    views: "45.6K",
+    comments: "820",
+  },
+  {
+    id: 3,
+    description:
+      "Bruce Wayne, also known as Batman, is a skilled investor and businessman who navigates the stock market with precision and insight, delivering exceptional results.",
+    name: "Bruce Wayne",
+    minutesAgo: 25,
+    profilePic:
+      "https://i.pinimg.com/originals/5e/f5/2e/5ef52e086b8d9a8a3a2f08a7851538f2.png",
+    likes:"2.4K",
+    views: "11.2K",
+    comments: "150",
+  },
+  {
+    id: 4,
+    description:
+      "Captain America, aka Steve Rogers, is a skilled trader who uses his strategic thinking to analyze market trends and make profitable trades.",
+    name: "Captain America",
+    minutesAgo: 49,
+    profilePic:
+      "https://i.pinimg.com/originals/5e/f5/2e/5ef52e086b8d9a8a3a2f08a7851538f2.png",
+    likes: "600",
+    views: "3.2K",
+    comments: "65",
+  },
+];
+
+export default discussionPostsData;
---
+++ src/app/marketData.js
@@ -0,0 +1,25 @@
+const market = [
+  {
+    id: 1,
+    topic: "Bullish Market",
+    description:
+      "Rising stock prices and overall optimistic sentiments in the market",
+    img: "https://www.investopedia.com/thumbs/stock-market/b/bear-vs-bull-market.jpg",
+  },
+  {
+    id: 2,
+    topic: "2023 Budget Proposal",
+    description:
+      "Budget 2023 proposal | Focus on sustainable growth and job creation: Finance Minister",
+    img: "https://www.financialexpress.com/wp-content/uploads/2023/01/Budget-2023-1.jpg",
+  },
+  {
+    id: 3,
+    topic: "Ethereum Rally",
+    description:
+      "Ethereum soars 10%; experts predict further growth in the coming weeks",
+    img: "https://cryptoslate.com/wp-content/uploads/2023/09/Ethereum-Price-Analysis-1.jpg",
+  },
+];
+
+export default market;
